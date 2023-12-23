d4a76652ec2f15f25312dfe9fcbb82fc2cd6d4ed
zyp <211250151@smail.nju.edu.cn>
2023-12-23 21:17:29 +0800

    master

---
+++ src/stores/user.ts
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia";
+import { request } from "~/utils/request";
+
+export const useUserStore = defineStore('user', {
+    state: () => {
+        return {
+            username: '',
+            name: '',
+            type: '',
+            idn: '',
+            phone: '',
+            credits: 0,
+            balance: 0
+        }
+    },
+    getters: {
+        getUserName() {
+
+        }
+    },
+    actions: {
+        fetch() {
+            request({
+                url: '/user',
+                method: 'GET'
+            }).then((res) => {
+                this.username = res.data.data.username;
+                this.name = res.data.data.name;
+                this.type = res.data.data.type;
+                this.idn = res.data.data.idn;
+                this.phone = res.data.data.phone;
+                this.credits = res.data.data.credits;
+                this.balance = res.data.data.balance;
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+
+})
